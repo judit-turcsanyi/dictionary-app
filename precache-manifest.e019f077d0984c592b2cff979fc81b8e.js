1f8d5cc73d145329123128e92ba7df7ae04d93fb
juditturcsanyi <judit.turcsanyi@prezi.com>
2023-09-13 22:03:15 +0200

    Updates

---
+++ precache-manifest.e019f077d0984c592b2cff979fc81b8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1af59bb66c6f7ee30eea",
+    "url": "/dictionary-app/static/css/main.33c3e6d2.chunk.css"
+  },
+  {
+    "revision": "1af59bb66c6f7ee30eea",
+    "url": "/dictionary-app/static/js/main.1af59bb6.chunk.js"
+  },
+  {
+    "revision": "e7a2d8aef8a281cc980c",
+    "url": "/dictionary-app/static/js/1.e7a2d8ae.chunk.js"
+  },
+  {
+    "revision": "f2b796ab099b1c9446b9",
+    "url": "/dictionary-app/static/js/runtime~main.f2b796ab.js"
+  },
+  {
+    "revision": "9e1e527ad7c32d1f89a3e79682116f46",
+    "url": "/dictionary-app/index.html"
+  }
+];
